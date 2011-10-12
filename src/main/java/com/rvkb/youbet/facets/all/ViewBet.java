2f501545285dfd8b74d860bb526fedd2f1563eed
Remi Vankeisbelck <remi@rvkb.com>
2011-10-12 22:54:48 +0200

    remove GAE files

--- src/main/java/com/rvkb/youbet/facets/all/ViewBet.java
+++
@@ -1,9 +0,0 @@
-package com.rvkb.youbet.facets.all;
-
-import com.rvkb.youbet.model.Bet;
-import net.sourceforge.jfacets.annotations.FacetKey;
-import woko.facets.builtin.developer.ViewImpl;
-
-@FacetKey(name="view", profileId = "all", targetObjectType = Bet.class)
-public class ViewBet extends ViewImpl {
-}
